51f546a6f7e304944c1e7e6d34a963028202a48a
Richard Fennell <richard@blackmarble.com>
2019-09-26 16:17:40 +0100

    Add the basic files

---
+++ README.md
@@ -0,0 +1,6 @@
+# ActionPlayground
+
+Trying out concepts with actions
+Edit1
+Edit more
+agai
---
+++ __tests__/first.test.ts
@@ -0,0 +1,12 @@
+describe("A basic test", () => {
+
+    it("should be able to add two numbers", () => {
+      // arrange
+
+      // act
+      var actual = 1 + 1;
+
+      // assert
+      expect(actual).toBe(2);
+    });
+})
